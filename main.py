8a5cab17763f81f3fe99b84917a9a82b2fdd5a99
iKapitonau <ilya.th.kapitonov@gmail.com>
2020-05-31 08:29:09 +0300

    Fix code style

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ import teacher
 import pupil
 
 if __name__ == '__main__':
-	app = QApplication(sys.argv)
-	dlg = auth.Dialog()
-	if not dlg.exec_():
-		# TODO: create teacher or pupil window
-		print(dlg.who)
+    app = QApplication(sys.argv)
+    dlg = auth.Dialog()
+    if not dlg.exec_():
+        # TODO: create teacher or pupil window
+        print(dlg.who)
